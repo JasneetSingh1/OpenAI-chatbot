928fb2fe588a40e304a0fd6be15338ad9510b43c
Jasneet <jasneetpalsingh619@gmail.com>
2024-03-20 20:27:10 -0600

    installed uvicorn and python-multipart

--- env/main.py
+++ env/main.py
@@ -1,4 +1,8 @@
 from openai import OpenAI
+from fastapi import FastAPI, Form
+from typing import Annotated
+
+app = FastAPI()
 
 openai = OpenAI(
     api_key="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"
@@ -6,20 +10,18 @@ openai = OpenAI(
 
 chatlog = []
 
-while True:
+@app.post("/")
+async def chat(user_input: Annotated(str, Form())):
 
-    user_input = input()
-    if user_input.lower() == 'stop':
-        break
 
     chatlog.append({'role': 'user', 'content': user_input})
 
     response = openai.chat.completions.create(
         model = 'gpt-3.5-turbo',
         messages = chatlog,
-        temperature= .6
+        temperature= 0.6
     )
     bot_response = response.choices[0].message.content
     chatlog.append({'role': 'assistant', 'content': bot_response})
-    print(bot_response)
+    return bot_response
 
