57ae97d0030f21c6ae17c0957ae470200513ac8c
Jasneet <jasneetpalsingh619@gmail.com>
2024-03-20 20:00:19 -0600

    added chat log which allowed the bot and the user to have a continious conversation

--- env/main.py
+++ env/main.py
@@ -1,16 +1,25 @@
 from openai import OpenAI
 
-
 openai = OpenAI(
-    api_key="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"
+    api_key="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"
 )
 
-response = openai.chat.completions.create(
-    model = 'gpt-3.5-turbo',
-    messages = [{
-        'role': 'system',
-        'content': 'You are a helpful assistant'
-    }, {}]
-)
+chatlog = []
 
-print(response.choices[0].message.content)
+while True:
+
+    user_input = input()
+    if user_input.lower() == 'stop':
+        break
+
+    chatlog.append({'role': 'user', 'content': user_input})
+
+    response = openai.chat.completions.create(
+        model = 'gpt-3.5-turbo',
+        messages = chatlog,
+        temperature= .6
+    )
+    bot_response = response.choices[0].message.content
+    chatlog.append({'role': 'assistant', 'content': bot_response})
+    print(bot_response)
+
